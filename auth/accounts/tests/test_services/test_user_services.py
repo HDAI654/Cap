272b02b95d84393feb04e852e722ea30d465ce10
HDAI654 <hdai.code@gmail.com>
2025-12-03 21:20:54 +0330

    improve user services tests

--- auth/accounts/tests/test_services/test_user_services.py
+++ auth/accounts/tests/test_services/test_user_services.py
@@ -1,16 +1,16 @@
-"""from django.test import TestCase
-from accounts.services.user_services import signup_user
+from django.test import TestCase
+from accounts.services.user_services import create_user
 from django.contrib.auth.models import User
 from unittest.mock import patch
 
+
 class UserServiceTest(TestCase):
     @patch("accounts.services.user_services.publish_user_created")
-    def test_signup_user_creates_user(self, mock_publish):
-        user = signup_user("serviceuser", "service@example.com", "strongpassword")
+    def test_create_user_creates_user(self, mock_publish):
+        user = create_user("serviceuser", "service@example.com", "strongpassword")
 
         self.assertEqual(user.username, "serviceuser")
         self.assertTrue(User.objects.filter(username="serviceuser").exists())
 
         # ensures Kafka function was triggered, but no real connection happens
         mock_publish.assert_called_once()
-"""
