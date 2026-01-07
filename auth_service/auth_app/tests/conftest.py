03c455b75601e99b9152b36f82ef8513203e4787
HDAI654 <hdai.code@gmail.com>
2026-01-07 19:53:02 +0330

    complete domain and infrastructure layers

--- auth_service/auth_app/tests/conftest.py
+++ auth_service/auth_app/tests/conftest.py
@@ -1,9 +1,1 @@
-import pytest
 
-
-@pytest.fixture(autouse=True)
-def disable_kafka(mocker):
-    mocker.patch(
-        "auth_app.infrastructure.messaging.kafka_producer.Producer",
-        autospec=True,
-    )
