4aed241b9d986a5edded2a046041be7aab36bac1
Hamed <kolbop.code@gmail.com>
2026-07-27 12:43:31 +0330

    complete release_holding service

---
+++ wallet_service/src/application/release_holding.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from src.domain.ports.unit_of_work import UnitOfWork
+from src.domain.value_objects.instrument_id import InstrumentId
+from src.domain.value_objects.quantity import Quantity
+from src.domain.value_objects.wallet_id import WalletId
+from src.exceptions import WalletNotFoundError
+
+
+@dataclass(frozen=True, slots=True)
+class ReleaseHoldingCommand:
+    """Input for the release-holding use case."""
+
+    wallet_id: str
+    instrument_id: str
+    quantity: int
+
+
+class ReleaseHoldingHandler:
+    """Application service that releases reserved shares in a wallet holding."""
+
+    def __init__(self, uow: UnitOfWork) -> None:
+        self._uow = uow
+
+    async def handle(self, command: ReleaseHoldingCommand) -> None:
+        """Release reserved shares in a holding of the given wallet."""
+        wallet_id = WalletId(command.wallet_id)
+        instrument_id = InstrumentId(command.instrument_id)
+        quantity = Quantity(command.quantity)
+
+        async with self._uow:
+            wallet = await self._uow.wallets.get_by_id(wallet_id)
+
+            wallet.release_holding(instrument_id, quantity)
+            await self._uow.wallets.update(wallet)
+            await self._uow.commit()
