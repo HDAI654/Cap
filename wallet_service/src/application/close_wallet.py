df0f70ad0423be154520ce481d6ff4397fa66cee
Hamed <kolbop.code@gmail.com>
2026-07-27 12:46:53 +0330

    complete close_wallet service

---
+++ wallet_service/src/application/close_wallet.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from src.domain.ports.unit_of_work import UnitOfWork
+from src.domain.value_objects.wallet_id import WalletId
+
+
+@dataclass(frozen=True, slots=True)
+class CloseWalletCommand:
+    """Input for the close-wallet use case."""
+
+    wallet_id: str
+
+
+class CloseWalletHandler:
+    """Application service that closes a wallet."""
+
+    def __init__(self, uow: UnitOfWork) -> None:
+        self._uow = uow
+
+    async def handle(self, command: CloseWalletCommand) -> None:
+        """Close the given wallet."""
+        wallet_id = WalletId(command.wallet_id)
+
+        async with self._uow:
+            wallet = await self._uow.wallets.get_by_id(wallet_id)
+
+            wallet.close()
+            await self._uow.wallets.update(wallet)
+            await self._uow.commit()
