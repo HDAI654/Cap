86e3aebe2eb0fc3ebf587976256d9de2cdef8651
Hamed <kolbop.code@gmail.com>
2026-07-27 12:45:29 +0330

    complete active_wallet service

---
+++ wallet_service/src/application/activate_wallet.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from src.domain.ports.unit_of_work import UnitOfWork
+from src.domain.value_objects.wallet_id import WalletId
+from src.exceptions import WalletNotFoundError
+
+
+@dataclass(frozen=True, slots=True)
+class ActivateWalletCommand:
+    """Input for the activate-wallet use case."""
+
+    wallet_id: str
+
+
+class ActivateWalletHandler:
+    """Application service that activates a wallet."""
+
+    def __init__(self, uow: UnitOfWork) -> None:
+        self._uow = uow
+
+    async def handle(self, command: ActivateWalletCommand) -> None:
+        """Activate the given wallet."""
+        wallet_id = WalletId(command.wallet_id)
+
+        async with self._uow:
+            wallet = await self._uow.wallets.get_by_id(wallet_id)
+
+            wallet.activate()
+            await self._uow.wallets.update(wallet)
+            await self._uow.commit()
