c31685a4c7e46cec7b09ae873366d1aba1e2ad7b
Hamed <kolbop.code@gmail.com>
2026-08-06 12:57:45 +0330

    auth-service: token-encoder & token-decoder ports is completed

---
+++ auth_service/src/domain/ports/token_encoder.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from src.domain.value_objects.device import Device
+from src.domain.value_objects.role import Role
+from src.domain.value_objects.session_id import SessionId
+from src.domain.value_objects.user_id import UserId
+
+
+class TokenEncoder(ABC):
+    """Creates signed access and refresh tokens."""
+
+    FIELD_TYPE_MAP: dict
+
+    @abstractmethod
+    def create_access_token(
+        self,
+        user_id: UserId,
+        session_id: SessionId,
+        device: Device,
+        role: Role | None = None,
+    ) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_refresh_token(
+        self,
+        user_id: UserId,
+        session_id: SessionId,
+        device: Device,
+        role: Role | None = None,
+    ) -> str:
+        raise NotImplementedError
